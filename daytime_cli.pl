6a3dc0562f8e9c378035b4a06f7a114fdee883ee
Ryan <jobs@boringtranquolity.io>
2020-10-26 09:23:25 -0700

    fix wording and remove empty lines

--- daytime_cli.pl
+++ daytime_cli.pl
@@ -1,9 +1,8 @@
 #!/usr/bin/env perl
 # file: daytime_cli.pl
-# CH. 3 (Intro to Berkeley sockets), "Network Programming with Perl"
 # https://tf.nist.gov/tf-cgi/servers.cgi
 
-use strict;
+use 5.30.1;
 use Socket;
 
 use constant DEFAULT_ADDR => '127.0.0.1';
@@ -11,7 +10,7 @@ use constant PORT	  => 13;
 use constant IPPROTO_TCP  => 6;
 
 my $address = shift || DEFAULT_ADDR;
-# in the call to inet_aton, a BAREWORD was originally used.
+# a BAREWORD was originally used in the call to inet_aton
 my $packed_addr = inet_aton($address);
 my $destination = sockaddr_in(PORT, $packed_addr);
 
@@ -19,5 +18,3 @@ socket(SOCK, PF_INET, SOCK_STREAM, IPPROTO_TCP) or die "Can't make socket: $!";
 connect(SOCK, $destination) or die "Can't connect: $!";
 
 print <SOCK>;
-
-
