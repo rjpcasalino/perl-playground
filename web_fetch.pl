1b5907d1a5da7818a0be4b6b34b4f53f7b9452cf
Ryan <jobs@boringtranquolity.io>
2020-10-27 16:50:36 -0700

    adds web fetch which only does http

---
+++ web_fetch.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/env perl
+# file: web_fetch.pl
+
+use v5.30.1;
+use IO::Socket qw(:DEFAULT :crlf);
+$/ = CRLF . CRLF;
+my $data;
+
+my $url = shift or die "Usage: web_fetch.pl <URL>\n";
+
+my ($host, $path) = $url=~m!^http://([^/]+)(/[^\#]*)! or die "Invalid URL\n";
+
+my $socket = IO::Socket::INET->new(PeerAddr => $host, PeerPort => 'http(80)') 
+	or die "Can't connect: $!";
+
+print $socket "GET $path HTTP/1.0",CRLF,CRLF;
+
+my $header = <$socket>; # read the header
+$header =~ s/$CRLF/\n/g;
+say $header;
+
+say $data while read($socket,$data,1024) > 0;
