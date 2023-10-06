3bcd132492246cef7e0217b36c147a7efa28892e
rjpc <ryan@rjpc.net>
2023-10-06 13:40:44 -0700

    remove shell nix and add flake nix

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A flake for perl playground";
+  nixConfig.bash-prompt = "[perl playground]$ $(pwd) ";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in with pkgs; {
+        devShell = mkShell {
+          buildInputs = [
+            pkgs.perl
+            pkgs.perlPackages.LWPProtocolHttps
+          ];
+          shellHook = ''
+            printf "Welcome to Perl Playground!\n"
+          '';
+        };
+      });
+}
