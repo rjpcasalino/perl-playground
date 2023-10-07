adf79dbb86fc5424a7ad4925e75f745252f5a248
rjpc <ryan@boringtranquility.io>
2023-10-07 09:24:33 -0700

    crude example of fetching all the rfcs; todo mak e it an option to list them

--- fetch_nix_rfcs.pl
+++ fetch_nix_rfcs.pl
@@ -4,8 +4,12 @@
 
 use v5.10;
 use warnings;
+use Data::Dumper;
 use LWP;
 use HTML::TokeParser::Simple;
+use JSON;
+use Encode qw(encode_utf8);
+
 use constant RFCS => 'https://raw.githubusercontent.com/NixOS/rfcs/master/rfcs/';
 
 die "Usage: fetch_nix_rfcs.pl 0001-rfc-process...\nGet full list: https://github.com/NixOS/rfcs/tree/master/rfcs\n" unless @ARGV;
@@ -14,12 +18,13 @@ my $p = HTML::TokeParser::Simple->new(url => "https://github.com/NixOS/rfcs/tree
 my $ua = LWP::UserAgent->new;
 my $new_agent = 'Mozilla/5.0 (' . $ua->agent . ')';
 $ua->agent($new_agent);
+my $data;
 
 while ( my $token = $p->get_token ) {
     # This prints all text in an HTML doc (i.e., it strips the HTML)
     next unless $token->is_text;
     # GitHub stores this shit in JSON payload on the page itself...
-    say $token->as_is if ($token->as_is =~ "payload");
+    $data = JSON->new->utf8->decode(encode_utf8($token->as_is)) if ($token->as_is =~ "payload");
     if (defined (my $rfc = shift)) {
 	my $url = RFCS . $rfc . ".md";
 	say "Fetching: $url";
@@ -27,4 +32,11 @@ while ( my $token = $p->get_token ) {
 	my $response = $ua->get($url);
 	say $response->decoded_content;
     }
+}
+my $rfcs = $data->{payload}{tree}{items};
+my $name = "0039-unprivileged-maintainer";
+foreach my $rfc (@$rfcs) {
+ my $rfc = Dumper($rfc->{name});
+ $Data::Dumper::Terse = 1;
+ say $rfc;
 }
