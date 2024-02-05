e3abb583a908a1b0e758ed29a31895c39b32f091
rjpc <ryan@rjpc.net>
2024-02-05 06:37:13 -0800

    add selenium and other packages

--- flake.nix
+++ flake.nix
@@ -8,15 +8,32 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in with pkgs; {
+        SeleniumRemoteDriver = pkgs.perlPackages.buildPerlPackage {
+          pname = "Selenium-Remote-Driver";
+          version = "1.49";
+          src = pkgs.fetchurl {
+            url = "mirror://cpan/authors/id/T/TE/TEODESIAN/Selenium-Remote-Driver-1.49.tar.gz";
+            hash = "sha256-yg7/7s6kK72vOVqI5j5EkoWKAAZAfJTRz8QY1BOX+mI=";
+          };
+          buildInputs = with pkgs.perlPackages; [ TestDeep TestFatal TestLWPUserAgent TestMockModule ];
+          propagatedBuildInputs = with pkgs.perlPackages; [ ArchiveZip Clone FileWhich HTTPMessage IOString JSON LWP Moo SubInstall TestLongString TryTiny XMLSimple namespaceclean ];
+          meta = {
+            homepage = "https://github.com/teodesian/Selenium-Remote-Driver";
+            description = "Perl Client for Selenium Remote Driver";
+          };
+        };
+      in
+      with pkgs; {
         devShell = mkShell {
           buildInputs = [
             pkgs.perl
-            pkgs.perlPackages.LWPProtocolHttps
-            pkgs.perlPackages.HTMLTokeParserSimple
-            pkgs.perlPackages.JSON
+            pkgs.perlPackages.TextCSV
             pkgs.perlPackages.Encode
+            pkgs.perlPackages.JSON
+            pkgs.perlPackages.LWPProtocolHttps
             pkgs.perlPackages.PerlTidy
+            pkgs.perlPackages.WebScraper
+            SeleniumRemoteDriver
           ];
           # LWP was throwing a warning about multiple shell vars
           # so shut it up like so:
